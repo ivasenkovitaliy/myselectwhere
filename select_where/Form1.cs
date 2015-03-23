b94fe137a74af980d1fa8191f144fd6337d057fa
Vitaliy Ivasenko <vitaliy.ivasenko@gmail.com>
2015-03-23 18:08:42 +0200

    my select and my where as extenthion methods

---
+++ select_where/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace select_where
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //var startList = new List<int>() {3,2,1,5};
+            var startList = new int[] {1, 2, 3, 5};
+
+            var select = startList.Select(n => n*n);
+            var mySelect = startList.MySelect(n => n*n);
+            
+            var where = startList.Where(n => n < 4);
+            var myWhere = startList.MyWhere(n => n < 4);
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ select_where/ext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace select_where
+{
+    public static class ext
+    {
+        public static IEnumerable<T> MySelect<T>(this IEnumerable<T> source, Func<T,T> func)
+        {
+            foreach (var value in source)
+            {
+                yield return func(value);
+            }
+            
+        }
+
+        public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> source, Func<T, bool> func)
+        {
+            foreach (var value in source)
+            {
+                if (func(value))
+                    yield return value;
+            }
+        } 
+    }
+}
